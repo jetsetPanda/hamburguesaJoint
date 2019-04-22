faa6f4eba2ca8bf893ccd8204ccd43a6bd54965a
jetsetPanda <marionong@gmail.com>
2019-04-21 23:29:17 -0400

    v.05

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger(burger_name, devoured) VALUES ('Big Mack', '1');
+INSERT INTO burger(burger_name, devoured) VALUES ('M. Bison Sliders','0');
+INSERT INTO burger(burger_name, devoured) VALUES ('Choppa Baconator', '0');
+INSERT INTO burger(burger_name, devoured) VALUES ('McDowells Triple Stack', '1');
+INSERT INTO burger(burger_name, devoured) VALUES ('Garden State VegBurg', '1');
+INSERT INTO burger(burger_name, devoured) VALUES ('Hamburgesa Supremo Loco', '0');
